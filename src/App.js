afcbbd2cd354218f0ec9b0c33e4e3b4150945c00
dikisetiawan1 <dikisetia101@gmail.comgit config --global user.email dikisetia101@gmail.comgit config --global user.email dikisetia101@gmail.com>
2024-05-26 14:22:17 +0700

    first upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Im Diki, I Learning react JS, and I promise as programmer front end until end. I fill, i can </h1>
+    </div>
+  );
+}
+
+export default App;
