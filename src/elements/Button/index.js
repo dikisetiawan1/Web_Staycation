3733697fe90204cd27782534acd08b1a69758384
Diki Setiawan <dikisetia101@gmail.com>
2024-06-09 13:14:51 +0700

    update hero

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -55,6 +55,7 @@ Button.propTypes = {
   href: propTypes.string,
   className: propTypes.string,
   isExternal: propTypes.string,
+  isPrimary: propTypes.bool,
   isDisabled: propTypes.bool,
   isLoading: propTypes.bool,
   isSmall: propTypes.bool,
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,21 +1,21 @@
 
 import Header from "parts/Header";
+import Hero from "parts/Hero";
+import landingPage from "json/landingPage.json";
 import { Component } from "react";
 
 
 
-class LandingPage  extends Component{
+export default class LandingPage  extends Component{
   render(){
 
     return (
     <>
-    <Header {...this.props}> </Header>
-    
+    <Header {...this.props}/> 
+    <Hero data={landingPage.hero} />
   
-    
     </>
   )
 }
 }
 
-export default (LandingPage)
